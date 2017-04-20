1fed4bf7ba0738a44757ec720385e4f2aa299171
Pedro-on-a-bike <backrowhero@gmail.com>
2017-04-20 15:26:59 -0400

    Update map.js

--- js/map.js
+++ js/map.js
@@ -15,7 +15,7 @@ function initialize() {
 		var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
 		
 		
-		$.getJSON("nodeList.json", function(data){
+		$.getJSON("nodeListTest.json", function(data){
 		
 		for (var key in data) {
 			var results = data[key];
